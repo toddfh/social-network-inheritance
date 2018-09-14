ac6667aea8b3d1ef8c60be3a32c3c293bfdbf4e2
Todd <appalachee.red@gmail.com>
2018-09-14 18:25:58 -0400

    All test passing.  Need to refactor

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,50 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = None
+
+    def set_user(self, user):
+        self.user = user
+
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        super(TextPost, self).__init__(text, timestamp)
+        self.text = text
+        self.timestamp = timestamp
+        self.readable_date = self.timestamp.strftime('%A, %b %d, %Y')
+
+    def __str__(self):
+        first_last = self.user.first_name + ' ' + self.user.last_name
+        return '@{}: "{}"\n\t{}'.format(first_last, self.text,
+                                        self.readable_date)
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        self.text = text
+        self.image_url = image_url
+        self.timestamp = timestamp
+        self.readable_date = self.timestamp.strftime('%A, %b %d, %Y')
+
+    def __str__(self):
+        first_last = self.user.first_name + ' ' + self.user.last_name
+        return '@{}: "{}"\n\t{}\n\t{}'.format(first_last, self.text,
+                                self.image_url, self.readable_date)
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        self.text = text
+        self.latitude = latitude
+        self.longitude = longitude
+        self.coords = str(self.latitude) + ', ' + str(self.longitude)
+        self.timestamp = timestamp
+        self.readable_date = self.timestamp.strftime('%A, %b %d, %Y')
 
-
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        return '@{} Checked In: "{}"\n\t{}\n\t{}'.format(self.user.first_name,
+        self.text, self.coords, self.readable_date)
